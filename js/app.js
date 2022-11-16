0a95a2a50d1a4d4d225dd191bb5649e9a183b387
Santy121655 <santylopes62@gmail.com>
2022-11-15 20:26:56 -0600

    api-y-js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import {modalBody, spinner , creaCard} from './ui.js';
+
 const urlBase = "https://rickandmortyapi.com/api/character/";
 
 const loadData = (urlBase, page = 1) => {
@@ -42,23 +44,6 @@ const showCharacters = (personajes) => {
   });
 };
 
-const creaCard = (personaje) => {
-  const html = `
-  <div class="card bg-dark text-light border" style="width: 18rem;">
-  <img src="${personaje.image}" class="card-img-top" alt="${personaje.image}">
-  <div class="card-body">
-    <h5 class="card-title">${personaje.name}</h5>
-    <p class="card-text">${personaje.status}</p>
-    <a href="#" 
-    class="btn btn-primary"
-    data-bs-toggle="modal"
-    data-bs-target="#exampleModal"
-    data-id="${personaje.id}">Ver más</a>
-  </div>
-</div>
-`;
-return html;
-}
 
 const navegacion = (e)  => {
   e.preventDefault();
@@ -68,17 +53,6 @@ const navegacion = (e)  => {
   }
 }
 
-const modalBody = (personaje) => {
-  const div = document.createElement('div');
-  div.classList.add('text-center');
-  let html = `` ;
-  html += `<img src="${personaje.image}" class="thumbnail">`;
-  html += `<p>${personaje.status}- ${personaje.species}</p>`;
-  html += `<p>Ultima ubicacion conocida</p> <p>${personaje.origin.name}</p>`;
-  html += `<p>Ha aparecido en ${personaje.episode.length}Episodios</p>`;
-  div.innerHTML = html;
-  return div;
-}
 
 const showCharactersById = (id) => {
   //se pedira la informacion de los personajes 
@@ -115,18 +89,7 @@ const loadInfo = (e) =>{
   }
 }
 
-const spinner = () =>{
-    const div = document.createElement('div');
-    const html =
-    `<div class="d-flex justify-content-center">
-    <div class="spinner-border" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  </div>`;
-  div.innerHTML = html;
-  return div;
 
-}
 
 
 document.querySelector('#botones').addEventListener('click', navegacion);
